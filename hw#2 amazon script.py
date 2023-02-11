837e9778a6507092d08786addaec103df6cd42ff
EA718 <EdwardAsh718@gmail.com>
2023-02-11 11:17:42 -0500

    hw#2 amazon script

---
+++ hw#2 amazon script.py
@@ -0,0 +1,29 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# init driver
+driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+driver.maximize_window()
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+service = Service('./chromedriver.exe')
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.amazon.com/')
+
+driver.find_element(By.XPATH, "//span[contains(text(), 'Orders') and @class='nav-line-2']").click()
+driver.find_element(By.ID, 'ap_email')
+
+expected_result = 'Sign in'
+actual_result = driver.find_element(By.XPATH, "//h1[contains(text(), 'Sign in')]").text
+print(actual_result)
+
+assert expected_result == actual_result, f'Expected {expected_result} but got actual {actual_result}'
+print('Test case passed')
+
+driver.quit()
--- sample_script.py
+++ sample_script.py
@@ -1,26 +1,26 @@
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+#from time import sleep
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
-driver.maximize_window()
+#driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+#driver.maximize_window()
 
 # open the url
-driver.get('https://www.google.com/')
+#driver.get('https://www.google.com/')
 
-search = driver.find_element(By.NAME, 'q')
-search.clear()
-search.send_keys('Dress')
+#search = driver.find_element(By.NAME, 'q')
+#search.clear()
+#search.send_keys('Dress')
 
 # wait for 4 sec
-sleep(4)
+#sleep(4)
 
 # click search
-driver.find_element(By.NAME, 'btnK').click()
+#driver.find_element(By.NAME, 'btnK').click()
 
 # verify
-assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
-print('Test Passed')
+#assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+#print('Test Passed')
 
-driver.quit()
+#driver.quit()
