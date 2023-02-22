07bdd1183e2eb054a43ea76a658b9c86c133555d
EA718 <EdwardAsh718@gmail.com>
2023-02-21 21:53:12 -0500

    Amazon empty cart

--- features/steps/Amazon Sign up locators HW#3.py
+++ features/steps/Amazon Sign up locators HW#3.py
@@ -1,32 +1,32 @@
-Amazon logo
+#Amazon logo
 #By CSS, using class
-driver.find_element(By.CSS_SELECTOR, '.a-icon.a-icon-logo')
+#driver.find_element(By.CSS_SELECTOR, '.a-icon.a-icon-logo')
 
-Create account
+#Create account
 #By CSS, using class
-driver.find_element(By.CSS_SELECTOR, 'h1.a-spacing-small')
+#driver.find_element(By.CSS_SELECTOR, 'h1.a-spacing-small')
 
-Name field
-driver.find_element(By.ID, 'ap_customer_name')
+#Name field
+#driver.find_element(By.ID, 'ap_customer_name')
 
-Email field
-driver.find_element(By.ID, 'ap_email')
+#Email field
+#driver.find_element(By.ID, 'ap_email')
 
 
-Password field
-driver.find_element(By.ID, 'ap-password')
+#Password field
+#driver.find_element(By.ID, 'ap-password')
 
-Passwords must be at least 6 characters.
-driver.find_element(By.Xpath, "//div[contains(text(), 'Passwords must be at least 6 characters.')]")
+#Passwords must be at least 6 characters.
+#driver.find_element(By.Xpath, "//div[contains(text(), 'Passwords must be at least 6 characters.')]")
 
-Reenter password field
-driver.find_element(By.ID, 'ap-password_check')
+#Reenter password field
+#driver.find_element(By.ID, 'ap-password_check')
 
-Continue link
-driver.find_element(By.ID, 'continue')
+#Continue link
+#driver.find_element(By.ID, 'continue')
 
-Conditions of Use
-driver.find_element(By.Xpath, "//a[contains(@href, ap_register_notification_condition_of_use')]")
+#Conditions of Use
+#driver.find_element(By.Xpath, "//a[contains(@href, ap_register_notification_condition_of_use')]")
 
-Privacy notice
-driver.find_element(By.Xpath, "//a[contains(@href, ap_register_notification_privacy_notice')]")
+#Privacy notice
+#driver.find_element(By.Xpath, "//a[contains(@href, ap_register_notification_privacy_notice')]")
--- features/steps/amazon_cart_emptyhw#3.py
+++ features/steps/amazon_cart_emptyhw#3.py
@@ -3,8 +3,7 @@ from behave import given, when, then
 
 cart_icon = (By.ID, 'nav-cart-count-container')
 amazon_cart_is_empty = (By.XPATH, "//div[@class='a-row sc-your-amazon-cart-is-empty']/h2")
-expected_result = "Your Amazon Cart is empty"
-
+expected_result = 'Your Amazon Cart is empty'
 
 @given('Open Amazon page')
 def open_amazon(context):
@@ -17,8 +16,8 @@ def click_cart_icon(context):
 @then('Verify that Your Amazon cart is empty is shown')
 def verify_amazon_cart_is_empty_result(context):
     context.driver.find_element(*amazon_cart_is_empty)
-
 @then('Verify that text {expected_result} is shown')
 def verify_expected_result(context, expected_result):
     actual_result = context.driver.find_element(By.XPATH, "//div[@class='a-row sc-your-amazon-cart-is-empty']/h2").text
-    assert expected_result == actual_result, f'Expected {expected_result} but got actual {actual_result}
+    assert expected_result == actual_result, f'Expected {expected_result} but got actual {actual_result}'
+
